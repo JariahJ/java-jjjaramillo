5ea58758ba44001215a59a7704e0159c2b2eb7d8
jaria <jaria@10.5.117.163>
2019-08-27 12:07:50 -0600

    says hi

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author jaria
+ */
+public class App {
+    
+    public static void main(String [] args) throws Exception {
+        App app = new App();
+        app.run();
+    }
+
+    private void run() {
+        System.out.println("Hello World"); 
+    }
+    
+}
