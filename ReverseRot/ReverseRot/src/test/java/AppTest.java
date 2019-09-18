89d53939e75d6eb0c2d1c869c0f0a70a308b0fda
jariah jaramillo <jariahjaramillo@yahoo.com>
2019-09-18 17:01:41 -0600

    tests updated

--- ReverseRot/ReverseRot/src/test/java/AppTest.java
+++ ReverseRot/ReverseRot/src/test/java/AppTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class AppTest {
    
 @Test
-  public void testReverseString(){
+  public void testRun(){
       App instance = new App();
       instance.input = "ABCDEFG";
       instance.numShifts = 1;
@@ -27,6 +27,54 @@ public class AppTest {
       assertEquals(expected,answer);
       
   }
-   
+  
+  @Test
+  public void testRun2(){
+      App instance = new App();
+      instance.input = "ZYXWV";
+      instance.numShifts = 3;
+      String expected = "YZ_.A";
+      String answer = instance.run(instance.a, instance.numShifts,instance.input);
+      assertEquals(expected,answer);
+      
+  }
+  
+  @Test
+  public void testRun3(){
+      App instance = new App();
+      instance.input = "AZIORLMFGN";
+      instance.numShifts = 7;
+      String expected = "UNMTSYVPEH";
+      String answer = instance.run(instance.a, instance.numShifts,instance.input);
+      assertEquals(expected,answer);
+      
+  }
+  
+  @Test
+  public void testReverse(){
+  App instance = new App();
+  instance.input = "ABCDE";
+  String expected = "EDCBA";
+  assertEquals(expected, instance.reverseString(instance.input));
+  
+  }
+  
+  @Test
+  public void testReverse2(){
+  App instance = new App();
+  instance.input = "Z_RFZZZNNOA";
+  String expected = "AONNZZZFR_Z";
+  assertEquals(expected, instance.reverseString(instance.input));
+  
+  }
+  
+  @Test
+  public void testReverse3(){
+  App instance = new App();
+  instance.input = "NGLASUZVMEQ";
+  String expected = "QEMVZUSALGN";
+  assertEquals(expected, instance.reverseString(instance.input));
+  
+  }
     
 }
